392e761d268075f9005401d5a9a3bfb1086abd57
Abdelrahmanman <AbdElRahman.man@hotmail.com>
2016-09-21 02:52:39 +0200

    design added

---
+++ js/tricks.js
@@ -0,0 +1,49 @@
+$(function () {
+    'use strict';
+    // add active class to links
+    $("ul li").click(function(){
+		$(this).addClass('active').siblings().removeClass('active');
+		})
+
+    // nice scroll with navbr links
+    $('.nav li a').click(function () {
+    $('html, body').animate({
+    scrollTop : $('#' + $(this).data('value')).offset().top}, 1000);
+    });
+
+    // btn to back to top
+    $(window).scroll(function(){
+			if($(this).scrollTop()>=400)
+				{$(".btntop").slideDown(1000);}
+			else
+				{$(".btntop").slideUp(1000);}
+		})
+
+    $('.btntop').click(function(){
+    $('html,body').animate({scrollTop : 0},1000);
+    })
+
+    $('.xspan').click(function(){
+      $('#about').slideUp(500, function(){
+        $('.fade').hide(500,function(){
+          $('body').removeClass('modal-open')
+        })
+      });
+    })
+
+    $('.xspann').click(function(){
+      $('#contact').slideUp(500, function(){
+        $('.fade').hide(500,function(){
+          $('body').removeClass('modal-open')
+        })
+      });
+    })
+
+    $(window).scroll(function(){
+			if($(this).scrollTop() == $('.skills').offset())
+				{
+          $('.skills').addClass('bounceInUp');
+        }
+			})
+
+    })
